723eaa28b37b313981302445be6cfce0ef184b81
Levente Krizsán <lewalevi@gmail.com>
2017-06-05 18:39:12 +0200

    Toggle switch test

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ First version of a small SPIFFS based webserver on the Esp8266 controller.
 Put all HTML/CSS/JS/Data files into their respectible folders in the data/ directory.
 If you want to create a new folder, you will have to add using the server.serveStatic() method in the setup() function.
 For example: server.serveStatic("/example/", SPIFFS, "/example/");
-A local WiFi SSID and password also have to be provided in the beginning of the program
+A local WiFi SSID and password also have to be provided in the beginning of the program.
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,27 @@
+/* INCLUDES	 */
 #include "Arduino.h"
+
+/* Libs for webserver and file system */
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <FS.h>
 
+/* Libs for display and connection */
+#include <Wire.h>
+#include "SSD1306.h"
+
+
 const char* ssid = "T-Home_4FE92A";
 const char* password = "33fvcuja";
 
 ESP8266WebServer server(80);
+SSD1306 display(0x3c, D1, D2);
 
-const int led = 13;
-
-void handleRoot() {
- 	digitalWrite(led, 1);
- 	server.send(200, "text/plain", "hello from esp8266!");
-	digitalWrite(led, 0);
-}
+uint8_t cnt = 0;
 
 void handleNotFound(){
-	digitalWrite(led, 1);
 	String message = "File Not Found\n\n";
 	message += "URI: ";
 	message += server.uri();
@@ -32,13 +34,20 @@ void handleNotFound(){
 	message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
 	}
 	server.send(404, "text/plain", message);
-	digitalWrite(led, 0);
 }
 
-void setup(void){
-	pinMode(led, OUTPUT);
-	digitalWrite(led, 0);
+void setup(){
 	Serial.begin(115200);
+
+	display.init();
+	display.clear();
+	display.display();
+	Serial.println("starting display");
+	display.setFont(ArialMT_Plain_24);
+	display.drawString(0, 26, String(cnt));
+	display.display();
+	Serial.println("drawing string");
+
 	Serial.println("Test");
 	WiFi.begin(ssid, password);
 	Serial.println("Starting setup");
@@ -56,7 +65,7 @@ void setup(void){
 	Serial.println(WiFi.localIP());
 
 	if (MDNS.begin("esp8266")) {
-	Serial.println("MDNS responder started");
+		Serial.println("MDNS responder started");
 	}
 
 	Serial.println("FS Init");
@@ -67,24 +76,19 @@ void setup(void){
 	server.serveStatic("/js/", SPIFFS, "/js/");
 	server.serveStatic("/fonts/", SPIFFS, "/fonts/");
 
-	Serial.println("Entry points");
-	/*server.on("/", handleRoot);
-
-	server.on("/inline", [](){
-	//	server.send(200, "text/plain", "this works as well");
-		server.send(200, "html", "<html><head><title>Esp8266 test</title></head><body><h1>This is a title</h1><p>This is a paragraph</p><ul><li>option 1</li><li>option 2</li><li>option 3</li></ul></body></html>");
-	})*/;
+	server.on("/toggle", [](){
+		display.clear();
+		display.drawString(0, 26, String(++cnt));
+		display.display();
+		//server.send(200, "text/plain", "this works as well");
+	});
 
 	server.onNotFound(handleNotFound);
 
-	Serial.println("Starting server on " + WiFi.localIP());
 	server.begin();
 	Serial.println("HTTP server started");
 }
 
-unsigned int cnt = 0;
-
-void loop(void){
+void loop(){
 	server.handleClient();
-
 }
